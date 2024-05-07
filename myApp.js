2b4fa9d2d1c65af60b46619f960f66252044ec5a
VijayaRatnaKumari <78976740+vijayaratnakumari@users.noreply.github.com>
2024-05-07 11:59:50 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,3 +2,7 @@ let express = require('express');
 let app = express();
 module.exports = app;
 console.log("Hello World")
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+    app.use("/public", express.static(__dirname + "/public"));
+  });
