26795aef2326885eb053f83b3d5e9f795f31be30
VijayaRatnaKumari <78976740+vijayaratnakumari@users.noreply.github.com>
2024-05-08 13:13:35 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,3 +6,27 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
     app.use("/public", express.static(__dirname + "/public"));
   });
+
+
+let express = require('express');
+let app = express();
+require('dotenv').config()
+
+// console.log(“Hello World”);
+
+app.get("/", (req,res) => {
+res.sendFile(__dirname + "/views/index.html")
+})
+
+// Assets at the /public route
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/json', (req, res) => {
+var response;
+if (process.env.MESSAGE_STYLE) {
+response = {"message": "HELLO JSON"}
+} else {
+response = {"message": "Hello json"}
+}
+res.json(response)
+});
